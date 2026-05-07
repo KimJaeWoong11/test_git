e6f014f188e43d1088d4d6662196e1245e12ecb9
김재웅 <nryun@suresofttech.com>
2026-05-07 11:28:31 +0900

    www

--- miro_vs2015/miro_fun.c
+++ miro_vs2015/miro_fun.c
@@ -114,7 +114,7 @@ void miro_way_find(link *tail ,char (*moving)[mx],int x,int y)
 	link *cross; /* 갈림길을 저장하기 위한 링크 포인터*/
 	
 	int num=0;
-
+	int test=0;
 	int end=0;
 	
 	cross=Q_make();/* cross위 head와 tail을 만든다.*/
