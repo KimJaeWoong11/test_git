294523731febcdac43e09d132b784116e6c3c478
김재웅 <nryun@suresofttech.com>
2026-05-07 11:23:04 +0900

    qw

--- miro_vs2015/Q_S.c
+++ miro_vs2015/Q_S.c
@@ -4,7 +4,7 @@
 int main()  
 {
 
-	char miro_map[my][mx]={				// 미로의 맵을 만든다.
+	char miro_map[my][mx]={				/* 미로의 맵을 만든다.*/
 		{wall,way,wall,'s',wall,wall,wall,wall,wall,wall,way,wall,wall,wall,wall,wall},
 		{wall,way,wall,way,way,way,way,way,wall,wall,wall,way,way,wall,wall,wall},
 		{wall,way,way,way,wall,wall,way,wall,way,wall,way,wall,way,way,way,way},
@@ -26,75 +26,75 @@ int main()
 
 
 
-char miro_map2[my][mx];//  맵을 복사해 놓는다.
+char miro_map2[my][mx];/*  맵을 복사해 놓는다.*/
 int count = (mx*my);
 
-char (*move)[mx]=miro_map;  // 맵의 첫 주소 값을 move 포인터에 다가 저장한다.
+char (*move)[mx]=miro_map;  /* 맵의 첫 주소 값을 move 포인터에 다가 저장한다.*/
 link *addr;
-//-------------------------------------------------변수선언의 끝 -----------------------------------------------//
+/*-------------------------------------------------변수선언의 끝 -----------------------------------------------//*/
 
 
-miro_map_cpy(miro_map,miro_map2);// 2차원 배열의 복사해준다.
+miro_map_cpy(miro_map,miro_map2);/* 2차원 배열의 복사해준다.*/
 	
 addr=Q_make();
 
-addr=Q_make_add(addr,count); // 맵의 x*Y만큼의 원형 연결 리스트를 만든다.
+addr=Q_make_add(addr,count); /* 맵의 x*Y만큼의 원형 연결 리스트를 만든다.*/
 
-map_out(miro_map); // 미로의 맵을 출력한다.
+map_out(miro_map); /* 미로의 맵을 출력한다.*/
 
-find_state(miro_map,addr);// 시작 좌표을 찾는다.
+find_state(miro_map,addr);/* 시작 좌표을 찾는다.*/
 
 printf("\n출발점: x = %d  y = %d  Enter을 누르면 길찾기를 시작합니다...\n",addr->prev->miro_x,addr->prev->miro_y);
 
-getchar();  //-test
+getchar();  /*-test*/
 
-miro_way_find(addr,move,addr->prev->miro_x,addr->prev->miro_y);//시작 값과 저장될 링크리스트만 값을 보내어 준다.
+miro_way_find(addr,move,addr->prev->miro_x,addr->prev->miro_y);/*시작 값과 저장될 링크리스트만 값을 보내어 준다.*/
 
-addr=Q_make_short(addr); //  최단 거리를 구한다.
-
-
-//--------------------------------------------------첫번째 길찾기의 끝 --------------------------------//
+addr=Q_make_short(addr); /*  최단 거리를 구한다.*/
 
 
+/*--------------------------------------------------첫번째 길찾기의 끝 --------------------------------//*/
 
-//-----------------------------------------------역으로 길찾기 시작-------------------------------------//
+
+
+/*-----------------------------------------------역으로 길찾기 시작-------------------------------------//*/
 getchar();
 
 system("cls");
 
-miro_map_cpy(miro_map2,miro_map);//맵을 초기화
+miro_map_cpy(miro_map2,miro_map);/*맵을 초기화*/
 
-map_out(miro_map); // 미로의 맵을 출력한다.(맵을 초기상태로 복원한것을 확인);
+map_out(miro_map); /* 미로의 맵을 출력한다.(맵을 초기상태로 복원한것을 확인);*/
 
-miro_way_completionQ(addr,move);//완성시킨 미로의 길이 저장된값과, 포인터 값을 넘겨주면 좀더 짧아진 길을 찍어준다.
+miro_way_completionQ(addr,move);/*완성시킨 미로의 길이 저장된값과, 포인터 값을 넘겨주면 좀더 짧아진 길을 찍어준다.*/
 
-map_out(miro_map); // 미로의 맵을 출력한다.
+map_out(miro_map); /* 미로의 맵을 출력한다.*/
 
 getchar();
 
-addr=map_re_find(addr);// 찾은 길을 더욱 줄여준다.  첫번째--
+addr=map_re_find(addr);/* 찾은 길을 더욱 줄여준다.  첫번째--*/
 
-miro_map_cpy(miro_map2,miro_map);//미로의 맵을 초기화
+miro_map_cpy(miro_map2,miro_map);/*미로의 맵을 초기화*/
 
-map_out(miro_map); //(맵을 초기상태로 복원한것을 확인);
+map_out(miro_map); /*(맵을 초기상태로 복원한것을 확인);*/
 
-getchar(); // 첫번째의 끝
+getchar(); /* 첫번째의 끝*/
 
-addr=map_re_find(addr);// 찾은 길을 더욱 줄여준다.  두번쨰--
+addr=map_re_find(addr);/* 찾은 길을 더욱 줄여준다.  두번쨰--*/
 
-//--------------------------------------------------------두번째 길찾기의 끝 --------------------------//
+/*--------------------------------------------------------두번째 길찾기의 끝 --------------------------//*/
 
 
 
-//----------------------------------------마지막 찾으길을 정리..--------------------------------------//
+/*----------------------------------------마지막 찾으길을 정리..--------------------------------------//*/
 
-miro_map_cpy(miro_map2,miro_map);// 2차원 배열의 복사해준다.
+miro_map_cpy(miro_map2,miro_map);/* 2차원 배열의 복사해준다.*/
 
-map_out(miro_map); // 미로의 맵을 출력한다.
+map_out(miro_map); /* 미로의 맵을 출력한다.*/
 
-miro_way_completionQ(addr,move);//최종 완성된 길을 찍어준다.
+miro_way_completionQ(addr,move);/*최종 완성된 길을 찍어준다.*/
 
-map_out(miro_map); //최종 길을 보여준다
+map_out(miro_map); /*최종 길을 보여준다*/
 
 printf("---------------길찾기를 완료하였습니다------------------\n");
 
@@ -103,7 +103,7 @@ return 0;
 
 }
 
-//---------------------------------------------------------------
+/*---------------------------------------------------------------*/
 
 
 
